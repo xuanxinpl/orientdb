852a81c016d6d3bd14f23140bd341fe58ecf3b91
lvca <l.garulli@gmail.com>
2015-08-17 14:32:02 +0200

    Created Travis only branch

--- core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OFullCheckpointRequestListener.java
+++
@@ -1,9 +0,0 @@
-package com.orientechnologies.orient.core.storage.impl.local;
-
-/**
- * @author Andrey Lomakin <a href="mailto:lomakin.andrey@gmail.com">Andrey Lomakin</a>
- * @since 05/02/15
- */
-public interface OFullCheckpointRequestListener {
-	void requestCheckpoint();
-}
